01b6fd937dde9f780cf22491114c5496a977668e
Matt <mattsnaccc@gmail.com>
2020-12-24 22:55:43 -0800

    file upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,71 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { FileDrop } from "react-file-drop";
+import { motion } from "framer-motion";
+import useSWR from "swr";
+import axios, { post } from "axios";
 
 export default function Home() {
+	const fetcher = (...args) => fetch(...args).then((r) => r.json());
+
+	const { data, error } = useSWR("/api/upload", fetcher);
+
+	if (error) console.log(error);
+
+	function fileUpload(files) {
+		const url = "http://localhost:3000/api/upload";
+		const formData = new FormData();
+		formData.append("file", files);
+		const config = {
+			headers: {
+				"content-type": "multipart/form-data",
+			},
+		};
+		return post(url, formData, config);
+	}
+
+	function handleDrop(files) {
+		console.log(files);
+		for (let i in files) {
+			let file_type = files[i].type;
+
+			let file_size = files[i].size;
+
+			if (
+				file_type === "image/png" ||
+				file_type === "image/jpeg" ||
+				file_type === "image/gif"
+			) {
+				if (file_size <= 25000000) {
+					fileUpload(files[i]);
+				} else {
+					document.querySelector(".upload").classList =
+						"upload duration-500 m-20 p-32 rounded-md border-4 border-dashed hover:border-solid border-red-300 hover:shadow-2xl cursor-pointer";
+				}
+			}
+		}
+	}
+
+	function handleStatus(call) {
+		if (call == "class") {
+			if (!data) {
+				return "bg-yellow-300 h-2 w-2 rounded-xl m-2 mt-3";
+			} else if (data.status == 200) {
+				return "bg-green-300 h-2 w-2 rounded-xl m-2 mt-3";
+			} else {
+				return "bg-red-300 h-2 w-2 rounded-xl m-2 mt-3";
+			}
+		} else {
+			if (!data) {
+				return "loading";
+			} else if (data.status == 200) {
+				return "online";
+			} else {
+				return "offline";
+			}
+		}
+	}
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -9,23 +73,49 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<section className="absolute top-8 left-8 flex flex-row justify-center items-center">
-				<div className="h-2 w-2 rounded-xl bg-green-300 m-2 mt-3" />
-				<span className="text-gray-500 cursor-default select-none">online</span>
-			</section>
+			<motion.section
+				initial={{ opacity: 0, y: -20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.3 }}
+				className="absolute top-8 left-8 flex flex-row justify-center items-center"
+			>
+				<div className={handleStatus("class")} />
+				<span className="text-gray-500 cursor-default select-none">
+					{handleStatus("text")}
+				</span>
+			</motion.section>
 
-			<header className="duration-300 m-2">
+			<motion.header
+				initial={{ opacity: 0, y: -20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.35 }}
+				className="duration-300 m-2"
+			>
 				<h1 className={styles.title}>Welcome.</h1>
 				<p className="text-gray-500 text-center">
 					Drop your files below to get started.
 				</p>
-			</header>
+			</motion.header>
 
-			<section className="duration-500 m-20 p-32 rounded-md border-4 border-dashed hover:border-solid border-gray-200 hover:shadow-2xl cursor-pointer">
-				<span className="text-gray-600">Drag & Drop files.</span>
-			</section>
+			<motion.section
+				initial={{ opacity: 0, y: 20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.4 }}
+			>
+				<FileDrop
+					className="upload duration-500 m-20 p-32 rounded-md border-4 border-dashed hover:border-solid border-gray-200 hover:shadow-2xl cursor-pointer"
+					onDrop={(files) => handleDrop(files)}
+				>
+					<span className="text-gray-600">Drag & Drop files.</span>
+				</FileDrop>
+			</motion.section>
 
-			<footer className={styles.footer}>
+			<motion.footer
+				initial={{ opacity: 0, y: 20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+				className={styles.footer}
+			>
 				<a
 					href="https://vercel.com?utm_source=cdont-ping.me&utm_medium=default-template&utm_campaign=dont-ping.me"
 					target="_blank"
@@ -34,7 +124,7 @@ export default function Home() {
 					Powered by{" "}
 					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
 				</a>
-			</footer>
+			</motion.footer>
 		</div>
 	);
 }
